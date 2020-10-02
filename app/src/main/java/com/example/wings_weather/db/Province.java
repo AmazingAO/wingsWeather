02d6e79e476129090048fb0b14d9c3db6910f562
张傲 <2490248693@qq.com>
2020-10-02 14:22:54 +0800

    db create

---
+++ app/src/main/java/com/example/wings_weather/db/Province.java
@@ -0,0 +1,33 @@
+package com.example.wings_weather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Province extends LitePalSupport {
+    private int id;
+    private String provinceName; //省份的名字
+    private int provinceCode; //省份的代号 由于和风天气中查询天气要使用对应省份的代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
